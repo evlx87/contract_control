d478c573b4fc4bdd78a013868261cac63ec0725a
evlx <evilnyagaming@yandex.ru>
2024-04-25 13:35:15 +0300

    создан base.html и обновлены html шаблоны

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from contracts.views import PurchaseListView, IndexView, AddContractView
+from contracts.views import PurchaseListView, IndexView, AddContractView, ContractDetailView, contract_detail
 
 app_name = 'contracts'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contracts/', PurchaseListView.as_view(), name='purchase_list'),
     path('add/', AddContractView.as_view(), name='add_contract'),
+    # path('contracts/<str:contract_number>/', ContractDetailView.as_view(), name='contract-detail'),
+    path('contracts/<str:contract_number>/', contract_detail, name='contract-detail'),
 ]
--- contracts/views.py
+++ contracts/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
-from contracts.forms import PaymentDocumentForm, ContractForm
-from contracts.models import Contract, PaymentDocument
+from contracts.forms import ContractForm
+from contracts.models import Contract
 
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'contracts/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Главная'
+        return context
 
-# def add_contract(request):
-#     if request.method == 'POST':
-#         form = ContractForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('/contracts/')  # Перенаправление на главную страницу после добавления, измените это по надобности
-#     else:
-#         form = ContractForm()
-#     return render(request, 'contracts/purchase_add.html', {'form': form})
+
 class AddContractView(View):
     def get(self, request):
         form = ContractForm()
@@ -38,3 +33,20 @@ class PurchaseListView(ListView):
     model = Contract
     template_name = 'contracts/purchases_list.html'  # Указываем имя шаблона
     context_object_name = 'purchases'  # Как будет называться переменная в шаблоне
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Закупки'
+        return context
+
+
+class ContractDetailView(DetailView):
+    model = Contract
+    template_name = 'contracts/contract_detail.html'
+    context_object_name = 'contract'
+    pk_url_kwarg = 'contract_number'
+
+
+def contract_detail(request, contract_number):
+    contract = Contract.objects.get(contract_number=contract_number)
+    return render(request, 'contracts/contract_detail.html', {'contract_detail': contract})
