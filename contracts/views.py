4e93d2b0a867b59c5827688ddbff0d8b6cd3cd09
evlx <evilnyagaming@yandex.ru>
2024-04-27 15:58:23 +0300

    справлена ошибка перенаправления после добавления платежного документа

--- contracts/views.py
+++ contracts/views.py
@@ -41,27 +41,31 @@ class PurchaseListView(ListView):
 
 
 def contract_detail(request, contract_number):
-    contract = Contract.objects.get(contract_number=contract_number)
+    contract = get_object_or_404(Contract, contract_number=contract_number)
     return render(request, 'contracts/contract_detail.html', {'contract_detail': contract})
 
 
 class AddPaymentDocView(View):
+    template_name = 'contracts/payment_doc_add.html'
+
+    def get_contract(self, contract_id):
+        return get_object_or_404(Contract, id=contract_id)
+
     def get(self, request, contract_id=None):
+        contract = None
         if contract_id:
-            contract = get_object_or_404(Contract, id=contract_id)
+            contract = self.get_contract(contract_id)
             form = PaymentDocumentForm(initial={'contract': contract})
         else:
             form = PaymentDocumentForm()
-        return render(
-            request, 'contracts/payment_doc_add.html', {'form': form, 'contract': contract})
+        return render(request, self.template_name, {'form': form, 'contract': contract})
 
     def post(self, request, contract_id):
-        contract = get_object_or_404(Contract, id=contract_id)
+        contract = self.get_contract(contract_id)
         form = PaymentDocumentForm(request.POST)
         if form.is_valid():
             payment_document = form.save(commit=False)
             payment_document.contract = contract
             payment_document.save()
-            return redirect('contract-detail', contract_number=contract.contract_number)
-        return render(
-            request, 'contracts/payment_doc_add.html', {'form': form, 'contract': contract})
+            return redirect('contracts:contract-detail', contract_number=contract.contract_number)
+        return render(request, self.template_name, {'form': form, 'contract': contract})
