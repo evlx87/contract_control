73078948b278948557165075a5c757ebd1fb0d42
evlx <evilnyagaming@yandex.ru>
2024-05-03 09:07:44 +0300

    создана модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('The Username field must be set')
+
+        user = self.model(
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+
+    # Добавляем related_name для разрешения конфликта имен
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.'
+    )
