17e3ee62d321de8a05e40d6f86955fa4ef953e4f
evlx <evilnyagaming@yandex.ru>
2024-05-06 15:00:34 +0300

    скорректирован формат ссылок, на странице контракта добавлена информация о доп.соглашениях по контракту

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from contracts.views import PurchaseListView, IndexView, AddContractView, contract_detail, AddPaymentDocView, \
     AddPaymentOrderView, contract_edit
@@ -11,6 +11,6 @@ urlpatterns = [
     path('add/', AddContractView.as_view(), name='add_contract'),
     path('contract/<int:pk>/', contract_detail, name='contract-detail'),
     path('contract/<int:contract_id>/edit/', contract_edit, name='contract-edit'),
-    path('add_doc/<int:contract_id>/', AddPaymentDocView.as_view(), name='add_payment_doc'),
-    path('add_order/<int:contract_id>/', AddPaymentOrderView.as_view(), name='add_payment_order')
+    path('contract/<int:contract_id>/add_doc', AddPaymentDocView.as_view(), name='add_payment_doc'),
+    path('contract/<int:contract_id>/add_order', AddPaymentOrderView.as_view(), name='add_payment_order')
 ]
