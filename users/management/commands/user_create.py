4b4574ed858c2407c8e80412b6553a5115cfd5dc
Sergey Semyonov <evilnyagaming@yandex.ru>
2024-09-21 20:59:17 +0300

    Обновлен скрипт создания пользователя и администратора портала

--- users/management/commands/user_create.py
+++ users/management/commands/user_create.py
@@ -4,19 +4,30 @@ from users.models import CustomUser
 
 
 class Command(BaseCommand):
-    help = 'Create a superuser'
+    help = 'Create a superuser and a regular user'
 
     def handle(self, *args, **options):
-        username = 'admin'  # Имя пользователя для создания
-        password = 'adminpassword'  # Пароль пользователя
+        # Создание суперпользователя
+        superuser_username = 'admin'
+        superuser_password = 'adminpassword'
+        superuser = CustomUser.objects.create_superuser(
+            username=superuser_username,
+            password=superuser_password)
+        superuser.is_superuser = True
+        superuser.is_staff = True
+        superuser.save()
 
-        # Создаем суперпользователя
+        self.stdout.write(self.style.SUCCESS(
+            f'Superuser {superuser_username} created successfully'))
+
+        # Создание обычного пользователя
+        user_username = 'user'
+        user_password = 'userpassword'
         user = CustomUser.objects.create_user(
-            username=username,
-            password=password)
-        user.is_superuser = True
-        user.is_staff = True
+            username=user_username,
+            password=user_password)
         user.save()
 
         self.stdout.write(self.style.SUCCESS(
-            f'Superuser {username} created successfully'))
+            f'User {user_username} created successfully'))
+
