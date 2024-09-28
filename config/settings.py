6a2aca898ea9e3aea13b0bd5124654cd707f64d1
evlx <evilnyagaming@yandex.ru>
2024-09-28 15:55:10 +0300

    allowed_hosts перенесен в .env

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1#&&%i=4_@9-n@z+5==hb(qm1t*pn63oxzhp@!@t3xb-w4oz5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
