03c0695459854f2447048c6e76bb2676c68e86ae
evlx <evilnyagaming@yandex.ru>
2024-10-01 17:29:36 +0300

    исправлен формат отображения логов

--- config/settings.py
+++ config/settings.py
@@ -148,7 +148,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s \t%(asctime)s \t%(module)s \t%(process)d \t%(thread)d \t%(message)s'
         },
     },
     'handlers': {
