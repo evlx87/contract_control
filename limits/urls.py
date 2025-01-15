9f45e3476e661530b88531c627a3bec448b6020f
evlx <evilnyagaming@yandex.ru>
2025-01-15 10:10:22 +0300

    исправлена опечатки в ссылке limits_list

--- limits/urls.py
+++ limits/urls.py
@@ -4,7 +4,7 @@ from limits.views import AddLimitView, LimitListView
 app_name = 'limits'
 
 urlpatterns = [
-    path('limit-list/', LimitListView.as_view(), name='limit_list'),
+    path('limits-list/', LimitListView.as_view(), name='limits_list'),
     path('add-limit/', AddLimitView.as_view(), name='add_limit'),
 ]
 
