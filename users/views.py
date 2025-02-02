4ff3a029ace7f740c9d987d067f8f8e9566f933d
evlx <evilnyagaming@yandex.ru>
2025-02-02 12:45:18 +0300

    views приложения users преобразованы в class

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
 
 # Create your views here.
-def custom_login_view(request):
-    if request.method == "POST":
+class CustomLoginView(View):
+    def get(self, request):
+        # Отобразить страницу входа
+        return render(request, 'registration/login.html')
+
+    def post(self, request):
+        # Обработка запроса на вход
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
@@ -15,5 +21,3 @@ def custom_login_view(request):
             return JsonResponse({"success": True})  # Успешный вход
         else:
             return JsonResponse({"error": "Пожалуйста, введите правильные имя пользователя и пароль."}, status=400)
-
-    return render(request, 'registration/login.html')
