e3dad46df97288760944d9401f9daa1806ab6334
evlx <evilnyagaming@yandex.ru>
2025-04-13 17:39:31 +0300

    Добавление итоговой строки (суммарные данные)

--- contracts/views.py
+++ contracts/views.py
@@ -3,6 +3,7 @@ import logging
 
 from django.contrib import messages
 from django.db import IntegrityError
+from django.db.models import Sum
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -64,34 +65,40 @@ class PurchaseListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         year = self.request.GET.get('year')
-
-        if not year:
-            year = timezone.now().year
-        queryset = queryset.filter(service_end_date__year=year)
         subject = self.request.GET.get('subject')
 
+        if year:
+            queryset = queryset.filter(service_end_date__year=year)
+        else:
+            year = timezone.now().year
+            queryset = queryset.filter(service_end_date__year=year)
         if subject:
             queryset = queryset.filter(contract_subject=subject)
 
-        return queryset.order_by('-contract_date')
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['page_title'] = 'Закупки'
-
-        # Получаем список всех годов
-        context['years'] = Contract.objects.order_by(
-            'service_end_date'
-        ).dates('service_end_date', 'year', order='ASC')
-
-        # Определяем текущий год
         current_year = timezone.now().year
-
-        # Если год не был передан через GET запрос, используем текущий год
-        context['selected_year'] = self.request.GET.get('year', current_year)
-        context['subjects'] = Contract.objects.values_list(
-            'contract_subject', flat=True).distinct()
+        years = Contract.objects.dates('service_end_date', 'year')
+        context['years'] = years
+        context['selected_year'] = self.request.GET.get('year', str(current_year))
+        subjects = Contract.objects.values_list('contract_subject', flat=True).distinct()
+        context['subjects'] = subjects
         context['selected_subject'] = self.request.GET.get('subject', '')
+
+        # Получаем все объекты после фильтрации
+        purchases = self.get_queryset()
+
+        # Вычисляем сумму contract_amount через aggregate
+        total_contract_amount = purchases.aggregate(Sum('contract_amount'))['contract_amount__sum'] or 0
+
+        # Вычисляем сумму total_pp_issued_amount вручную
+        total_payment_amount = sum(purchase.total_pp_issued_amount for purchase in purchases) or 0
+
+        context['total_contract_amount'] = total_contract_amount
+        context['total_payment_amount'] = total_payment_amount
+
         return context
 
 
